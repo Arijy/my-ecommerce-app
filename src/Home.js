8475389923dfea3a49b63f45ad3eab2898fbe04b
Arijy <arijchaudhary@gmail.com>
2020-03-21 23:31:22 +0500

    my commit

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Component/Header'
+import Body from './Component/Body'
+import Footer from './Component/Footer'
+
+class Home extends React.component{
+ render() 
+ {
+    return(
+        
+            <div>
+
+               <Header/>
+                 <Body/>
+                 <Footer/>
+                 </div>
+        
+               
+        
+           )
+     }
+}
+export default Home;
